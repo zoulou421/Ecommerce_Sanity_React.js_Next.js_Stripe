5bc21b704105f75c532f6b3f9d6bb97013129db0
Bonevy BEBY <bonevybeby@gmail.com>
2022-09-24 18:27:24 +0200

    Create a backend side using SANITY NEXT.js and also display the index.js

---
+++ sanity_ecommerce/schemas/banner.js
@@ -0,0 +1,67 @@
+export default {
+	name:'banner',
+	title:'Banner',
+	type:'document',
+	fields: [
+      {
+      	name:'image',
+      	title:'Image',
+      	type:'image',
+      	options:{
+      		hotspot:true,
+      	}
+      },
+      {
+      	name:'buttonText',
+      	title:'ButtonText',
+      	type:'string',
+      },
+      {
+      	name:'product',
+      	title:'Product',
+      	type:'string',
+      },
+
+      {
+      	name:'desc',
+      	title:'Desc',
+      	type:'string',
+      },
+      {
+       name:'smallText',
+       title:'MidText',
+       type:'string',
+      },
+
+      {
+       name:'largelText1',
+       title:'LargelText1',
+       type:'string',
+      },
+      {
+       name:'largelText2',
+       title:'LargelText2',
+       type:'string',
+      },
+
+      {
+       name:'discount',
+       title:'Discount',
+       type:'string',
+      },
+      {
+       name:'saleTime',
+       title:'SaleTime',
+       type:'string',
+      },
+
+	],
+};
+
+
+
+
+
+
+
+
